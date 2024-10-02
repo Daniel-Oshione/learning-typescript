42222ad248a651eb1bc8e3c6bf251637751fa03e
Daniel-Oshione <omobsdaniel@gmail.com>
2024-10-02 13:32:21 +0100

    learning typescript syntax

--- variable/variable.js
+++ variable/variable.js
@@ -1,3 +1,23 @@
-async function hello() {
-    return `world`;
+"import _ from 'lodash';";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello() {
+    return "world";
 }
+var humanA = {
+    first: 'Dan',
+    last: 'Might',
+    age: 30
+};
+var humanB = {
+    first: 'Chick',
+    last: 'Might',
+    age: 25
+};
+console.log(humanA.age);
+// array stongtyping
+var arr = [];
+arr.push(1);
+arr.push(5);
+arr.push(true);
+console.log(arr);
--- variable/variable.ts
+++ variable/variable.ts
@@ -1,3 +1,36 @@
-async function hello() {
-    return `world`
+import { first } from "lodash";
+
+`import _ from 'lodash';`
+
+function hello() {
+    return `world`;
 }
+
+//stongtyping in typescript
+interface Attributes{
+    first: string,
+    last: string,
+    age: number
+}
+
+const humanA : Attributes = {
+    first: 'Dan',
+    last: 'Might',
+    age: 30
+}
+const humanB : Attributes = {
+    first: 'Chick',
+    last: 'Might',
+    age: 25
+}
+
+console.log(humanA.age);
+
+// array stongtyping
+const arr: number[] = [];
+
+arr.push(1);
+arr.push(5);
+arr.push(3);
+
+console.log(arr);
